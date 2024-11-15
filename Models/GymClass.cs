6e6794a32ebe6d886a006a0c5b05a63533ff4bba
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-15 14:09:37 +0100

    Added GymClass

---
+++ Models/GymClass.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingApp.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; } 
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; }
+    }
+}
