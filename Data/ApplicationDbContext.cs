1d5eb7189eb081a7611f404a6f67e98c861d1d4c
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-15 14:23:50 +0100

    Added Composition key with FluentAPI

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,5 +12,12 @@ namespace BookingApp.Data
         }
 
         public DbSet<GymClass> GymClasses { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUserGymClass>()
+                .HasKey(x => new {x.GymClassId, x.ApplicationUserId});
+        }
     }
 }
