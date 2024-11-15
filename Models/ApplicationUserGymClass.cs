086bb8953e05d7e20e67aeb523881ed5036b45ad
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-15 14:16:05 +0100

    Added ApplicationUserGymClass

---
+++ Models/ApplicationUserGymClass.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingApp.Models
+{
+    public class ApplicationUserGymClass
+    {
+        public int GymClassId {  get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public GymClass Class { get; set; }
+        public ApplicationUser User { get; set; } 
+    }
+}
